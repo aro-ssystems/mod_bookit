52204749c8d5ff5215d04f9c922d3f00a3b90cd0
Andreas Rosenthal <arosenthal@ssystems.de>
2026-03-06 13:02:06 +0100

    fix: use https in license URLs in resource admin pages

--- admin/resource_checklist.php
+++ admin/resource_checklist.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - https://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * Admin page for managing resource checklist.
@@ -20,7 +20,7 @@
  * @package     mod_bookit
  * @copyright   2026 ssystems GmbH <oss@ssystems.de>
  * @author      Andreas Rosenthal
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 use mod_bookit\local\tabs;
